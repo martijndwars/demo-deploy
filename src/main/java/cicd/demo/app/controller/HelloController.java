52c83f8e3b54ac85b8061068e51bedba757eb9b5
Martijn Dwars <martijn.dwars@oracle.com>
2020-12-09 12:47:25 +0100

    Do not expect Guava to be present

--- src/main/java/cicd/demo/app/controller/HelloController.java
+++ src/main/java/cicd/demo/app/controller/HelloController.java
@@ -15,11 +15,13 @@ public class HelloController {
     public String index() throws IOException {
         // Read META-INF/maven/com.google.guava/guava/pom.properties from classpath
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream("META-INF/maven/com.google.guava/guava/pom.properties");
-        Properties properties = new Properties();
-        properties.load(inputStream);
-        String guavaVersion = properties.getProperty("version");
-        if (!guavaVersion.equals("23.6-jre")) {
-            throw new RuntimeException("Expected Guava 23.6-jre, but got: " + guavaVersion);
+        if (inputStream != null) {
+            Properties properties = new Properties();
+            properties.load(inputStream);
+            String guavaVersion = properties.getProperty("version");
+            if (!guavaVersion.equals("23.6-jre")) {
+                throw new RuntimeException("Expected Guava 23.6-jre, but got: " + guavaVersion);
+            }
         }
         return "Hello World 1";
     }
