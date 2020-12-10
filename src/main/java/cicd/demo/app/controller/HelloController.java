7cdced2cee25060521d318478f4d60b5dab817fa
Issam LANSSARI <issam.lanssari@oracle.com>
2020-12-10 09:33:07 +0100

    ** fix hooks test

--- src/main/java/cicd/demo/app/controller/HelloController.java
+++ src/main/java/cicd/demo/app/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @Get(produces = MediaType.TEXT_PLAIN)
     public String index() throws IOException {
         // Read META-INF/maven/com.google.guava/guava/pom.properties from classpath
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("META-INF/maven/com.google.guava/guava/pom.properties");
+        /*InputStream inputStream = getClass().getClassLoader().getResourceAsStream("META-INF/maven/com.google.guava/guava/pom.properties");
         if (inputStream != null) {
             Properties properties = new Properties();
             properties.load(inputStream);
@@ -22,6 +22,13 @@ public class HelloController {
             if (!guavaVersion.equals("23.6-jre")) {
                 throw new RuntimeException("Expected Guava 23.6-jre, but got: " + guavaVersion);
             }
+        }*/
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("META-INF/maven/com.google.guava/guava/pom.properties");
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        String guavaVersion = properties.getProperty("version");
+        if (!guavaVersion.equals("23.6-jre")) {
+            throw new RuntimeException("Expected Guava 23.6-jre, but got: " + guavaVersion);
         }
         return "Hello World 1";
     }
