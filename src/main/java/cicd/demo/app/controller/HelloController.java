df63a518c92839e4778386fbb83e390274090956
Martijn Dwars <martijn.dwars@oracle.com>
2020-12-16 17:33:21 +0100

    Change 11

--- src/main/java/cicd/demo/app/controller/HelloController.java
+++ src/main/java/cicd/demo/app/controller/HelloController.java
@@ -23,6 +23,6 @@ public class HelloController {
                 throw new RuntimeException("Expected Guava 23.6-jre, but got: " + guavaVersion);
             }
         }
-        return "Hello World 105";
+        return "Hello World 106";
     }
 }
